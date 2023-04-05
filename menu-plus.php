5c8b3e967a6d0c89e65c5df94fccfe56031d00b0
Rafael Nowrotek <rexblack@users.noreply.github.com>
2023-04-05 16:01:28 +0200

    Bump version

--- menu-plus.php
+++ menu-plus.php
@@ -3,7 +3,7 @@
  Plugin Name: Menu Plus
  Plugin URI: http://github.com/benignware/wp-menu-plus
  Description: Enhance menus with buttons and search
- Version: 1.0.0-beta.4
+ Version: 1.0.0-beta.5
  Author: Rafael Nowrotek, Benignware
  Author URI: http://benignware.com
  License: MIT
